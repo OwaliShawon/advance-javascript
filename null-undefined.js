c25601231bbf107af9860eaafbcdc5ab8c890910
Owali Ullah Shawon <owali.shawon@gmail.com>
2021-02-02 19:44:02 +0600

    null vs undefined

---
+++ null-undefined.js
@@ -0,0 +1,33 @@
+// let pakhi;
+// console.log(pakhi);
+
+
+
+// function add(num1, num2){
+//     console.log(num1 + num2);
+// }
+
+// const result = add(13, 12);
+// console.log(result);
+
+
+
+// function add(num1, num2 = 10){
+//     console.log(num1 + num2);
+// }
+
+// const result = add(13);
+// console.log(result);
+
+
+// const premik = {name: "ssmart boy", phone: 912498723}
+// console.log(premik.gf);
+
+
+// let fun = undefined;
+// console.log(fun);
+
+
+
+let ages = [1, 34, 34, 2];
+console.log(ages[10]);
