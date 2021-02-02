071e76cebd3342ea33b43cb3dfbe274d00d3d804
Owali Ullah Shawon <owali.shawon@gmail.com>
2021-02-03 00:12:42 +0600

    array split

--- array-split.js
+++ array-split.js
@@ -1,1 +1,8 @@
 const nums = [1, 2, 3, 4, 5, 6, 7, 8]
+// const half = nums.slice(2, 5);
+const remove = nums.splice(2, 5, 71);
+ console.log(remove);
+ console.log(nums);
+
+ const together = nums.join(" , ");
+ console.log(together);
