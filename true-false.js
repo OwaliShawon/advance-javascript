d42bb3d04564a59fd5b6983d26698764a031df36
Owali Ullah Shawon <owali.shawon@gmail.com>
2021-02-02 19:19:39 +0600

    true false

---
+++ true-false.js
@@ -0,0 +1,16 @@
+// falsy
+// 0 false
+// empty string false
+// null false
+// NaN false
+
+
+
+
+const age = NaN;
+if(age){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
